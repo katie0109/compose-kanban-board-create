cffdce5eb34425faca500dbc9aae0e6e58cd1dfc
nadajinny <jinsun23007@gmail.com>
2026-03-10 16:39:58 +0900

    feat: 설명 입력 칸의 "설명 표시", 입력받는 박스 생성 및 사전 문구 추가

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/taskForm/DescriptionInputSection.kt
@@ -0,0 +1,64 @@
+package woowacourse.kanban.board.ui.taskForm
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.design.Font
+import woowacourse.kanban.board.model.Description
+
+@Composable
+fun DescriptionInputSection() {
+    Column {
+        Text(
+            text = "설명",
+            fontSize = Font.FORMTITLE.size,
+            fontWeight = Font.FORMTITLE.weight,
+            modifier = Modifier.padding(8.dp),
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        DescriptionInputField()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun DescriptionInputPreview() {
+    MaterialTheme {
+        DescriptionInputSection()
+    }
+}
+
+@Composable
+private fun DescriptionInputField() {
+    var description: String by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = description,
+        onValueChange = {
+            description = it
+        },
+        placeholder = {
+            Text(
+                text = "태스크에 대한 자세한 설명을 입력하세요",
+                fontSize = Font.FORMINPUT.size,
+                fontWeight = Font.FORMINPUT.weight
+            )
+        },
+        modifier = Modifier.height(150.dp)
+    )
+}
