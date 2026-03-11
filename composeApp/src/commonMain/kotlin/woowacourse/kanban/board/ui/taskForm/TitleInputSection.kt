590e2dccec67a1f99bd5e73744d04895231cba31
nadajinny <jinsun23007@gmail.com>
2026-03-11 17:12:50 +0900

    feat: 제목이 작성되지 않았을 경우를 위한 경고 문구

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/taskForm/TitleInputSection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/taskForm/TitleInputSection.kt
@@ -14,11 +14,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,6 +32,7 @@ import woowacourse.kanban.board.design.Font
 
 @Composable
 fun TitleInputSection() {
+    val isFilled = remember{mutableStateOf(false)}
     Column {
         Text(
             text = "제목 *",
@@ -37,7 +40,6 @@ fun TitleInputSection() {
             fontWeight = Font.FORMTITLE.weight,
             modifier = Modifier.padding(8.dp).fillMaxWidth(),
         )
-        TitleInputField()
     }
 }
 
@@ -50,13 +52,23 @@ private fun TitleInputPreview() {
 }
 
 @Composable
-private fun TitleInputField() {
+private fun TitleInputField(onCheckFilled:(Boolean) -> Unit) {
     var title: String by remember { mutableStateOf("") }
+    var isEmptyError by remember { mutableStateOf(false) }
+    var isFocused by remember { mutableStateOf(false) }
+    val supportingText by remember {
+        derivedStateOf {
+            if(isEmptyError) "제목을 입력해주세요"
+            else ""
+        }
+    }
     OutlinedTextField(
         value = title,
         onValueChange = {
             title = it
+            onCheckFilled(it.isNotEmpty())
         },
+        isError = isEmptyError,
         placeholder = {
             Text(
                 text = "태스크 제목을 입력하세요",
@@ -65,6 +77,20 @@ private fun TitleInputField() {
                 color = Color(0xFFAAAAAA)
             )
         },
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged { focusState ->
+                isFocused = focusState.isFocused
+                if(!isFocused&&title.isEmpty()) isEmptyError = true
+                else isEmptyError= false
+            },
+        supportingText = {
+            Text(
+                text = supportingText,
+                fontSize = Font.FORMEXPLAIN.size,
+                fontWeight = Font.FORMEXPLAIN.weight
+            )
+        },
+
     )
 }
