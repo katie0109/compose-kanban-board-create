2c765184d672e63460c7ba8f3aa7b38ea1bf5a43
nadajinny <jinsun23007@gmail.com>
2026-03-11 14:16:04 +0900

    feat: 박스 비율이 커지도록 조정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/taskForm/AssigneeInputSection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/taskForm/AssigneeInputSection.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,12 +23,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.design.Font
 import woowacourse.kanban.board.model.Assignee
 import woowacourse.kanban.board.ui.AssigneeSection
 
+
+@Composable
+fun AssigneeInputSection() {
+    val assignees = listOf(
+        Assignee("다이노"),
+        Assignee("페임스")
+    )
+    var selected by remember { mutableStateOf(assignees.first()) }
+    Column {
+        Text(
+            text = "담당자 *",
+            fontSize = Font.FORMTITLE.size,
+            fontWeight = Font.FORMTITLE.weight,
+            modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        )
+        AssigneeField(
+            assignees = assignees,
+            selected = selected,
+            onSelect = { selected = it }
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-private fun AssigneeInputSection() {
+private fun AssigneeInputSectionPreview() {
     val assignees = listOf(
         Assignee("다이노"),
         Assignee("페임스")
@@ -47,7 +75,7 @@ fun AssigneeField(
 ) {
     Row(
         modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         assignees.forEach { assignee ->
             val isSelected = assignee == selected
@@ -55,6 +83,7 @@ fun AssigneeField(
             val backgroundColor = if(isSelected) Color(0xFFEEF2FF) else Color(0xFFFFFFFF)
             Box(
                 modifier = Modifier
+                    .weight(1f)
                     .border(
                         width = 2.dp,
                         color = borderColor,
@@ -69,5 +98,8 @@ fun AssigneeField(
                 AssigneeSection(assignee)
             }
         }
+        Spacer(
+            modifier = Modifier.weight(1f)
+        )
     }
 }
