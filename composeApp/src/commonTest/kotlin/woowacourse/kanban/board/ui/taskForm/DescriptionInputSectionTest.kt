da1c0dd42d4206adf419221c425bfa96949a1507
nadajinny <jinsun23007@gmail.com>
2026-03-12 11:07:22 +0900

    feat: 설명 입력 칸에서 진행해야하는 테스트 케이스 추가

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/taskForm/DescriptionInputSectionTest.kt
@@ -0,0 +1,35 @@
+package woowacourse.kanban.board.ui.taskForm
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasSetTextAction
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@OptIn(ExperimentalTestApi::class)
+class DescriptionInputSectionTest {
+    @Test
+    fun `설명 입력은 상태를 갱신한다`() = runComposeUiTest {
+        var description by mutableStateOf("")
+
+        setContent {
+            MaterialTheme {
+                DescriptionInputSection(
+                    description = description,
+                    onDescriptionChange = { description = it },
+                )
+            }
+        }
+
+        onNode(hasSetTextAction()).performTextInput("상세 설명")
+
+        runOnIdle {
+            assertEquals("상세 설명", description)
+        }
+    }
+}
