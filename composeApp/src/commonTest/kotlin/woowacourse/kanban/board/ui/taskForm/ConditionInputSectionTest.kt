98b65cffebec39a8bb109ef0374403e4845847e8
nadajinny <jinsun23007@gmail.com>
2026-03-12 14:00:45 +0900

    feat: 상태 입력란 테스트 케이스 추가

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/taskForm/ConditionInputSectionTest.kt
@@ -0,0 +1,37 @@
+﻿package woowacourse.kanban.board.ui.taskForm
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class ConditionInputSectionTest {
+
+    @Test
+    fun `화면에서 상태가 모두 표시된다`() = runComposeUiTest {
+        setContent { ConditionInputSection() }
+
+        onNodeWithText("To Do").assertIsDisplayed()
+        onNodeWithText("In Progress").assertIsDisplayed()
+        onNodeWithText("Done").assertIsDisplayed()
+    }
+
+    @Test
+    fun `TODO 상태가 기본 선택값으로 렌더링된다`() = runComposeUiTest {
+        setContent { ConditionInputSection() }
+
+        onNodeWithText("To Do").assertIsDisplayed().assertHasClickAction()
+    }
+
+    @Test
+    fun `In Progress 버튼을 클릭하면 클릭 이벤트가 동작한다`() = runComposeUiTest {
+        setContent { ConditionInputSection() }
+
+        onNodeWithText("In Progress").performClick()
+        onNodeWithText("In Progress").assertIsDisplayed()
+    }
+}
