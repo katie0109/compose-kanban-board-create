03ca6148655ff3a9f9e539895617c0610827fb21
katie0109 <0109katie@naver.com>
2026-03-16 15:07:42 +0900

    refactor: 객체명 수정

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/taskCardForm/TaskStatusInputSectionTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/taskCardForm/TaskStatusInputSectionTest.kt
@@ -1,19 +1,32 @@
 ﻿package woowacourse.kanban.board.ui.taskCardForm
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runComposeUiTest
+import woowacourse.kanban.board.model.TaskStatus
 import kotlin.test.Test
 
 @OptIn(ExperimentalTestApi::class)
-class ConditionInputSectionTest {
+class TaskStatusInputSectionTest {
 
     @Test
     fun `화면에서 상태가 모두 표시된다`() = runComposeUiTest {
-        setContent { ConditionInputSection() }
+
+        setContent {
+            var selectedTaskStatus by remember { mutableStateOf(TaskStatus.TODO) }
+
+            TaskStatusInputSection(
+                selectedTaskStatus = selectedTaskStatus,
+                onStatusChange = { selectedTaskStatus = it }
+            )
+        }
 
         onNodeWithText("To Do").assertIsDisplayed()
         onNodeWithText("In Progress").assertIsDisplayed()
@@ -22,14 +35,30 @@ class ConditionInputSectionTest {
 
     @Test
     fun `TODO 상태가 기본 선택값으로 렌더링된다`() = runComposeUiTest {
-        setContent { ConditionInputSection() }
+
+        setContent {
+            var selectedTaskStatus by remember { mutableStateOf(TaskStatus.TODO) }
+
+            TaskStatusInputSection(
+                selectedTaskStatus = selectedTaskStatus,
+                onStatusChange = { selectedTaskStatus = it }
+            )
+        }
 
         onNodeWithText("To Do").assertIsDisplayed().assertHasClickAction()
     }
 
     @Test
     fun `In Progress 버튼을 클릭하면 클릭 이벤트가 동작한다`() = runComposeUiTest {
-        setContent { ConditionInputSection() }
+
+        setContent {
+            var selectedTaskStatus by remember { mutableStateOf(TaskStatus.TODO) }
+
+            TaskStatusInputSection(
+                selectedTaskStatus = selectedTaskStatus,
+                onStatusChange = { selectedTaskStatus = it }
+            )
+        }
 
         onNodeWithText("In Progress").performClick()
         onNodeWithText("In Progress").assertIsDisplayed()
