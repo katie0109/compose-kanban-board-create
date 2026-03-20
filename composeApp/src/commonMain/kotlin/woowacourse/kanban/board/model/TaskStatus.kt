1ce24265daa6dfd1fee4309e9a61663b8f5cf9ac
katie0109 <0109katie@naver.com>
2026-03-20 23:45:19 +0900

    refactor: 상태별 보드 색상 매핑 함수 정리

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/TaskStatus.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/TaskStatus.kt
@@ -1,7 +1,51 @@
 package woowacourse.kanban.board.model
 
+import androidx.compose.ui.graphics.Color
+
 enum class TaskStatus(val text: String) {
     TODO("To Do"),
     INPROGRESS("In Progress"),
     DONE("Done")
+}
+
+fun statusToHeaderColor(status: TaskStatus): Color{
+    return when(status){
+        TaskStatus.TODO -> {
+            Color(0xFF155DFC)
+        }
+        TaskStatus.INPROGRESS -> {
+            Color(0xFFE17100)
+        }
+        TaskStatus.DONE -> {
+            Color(0xFF00A63E)
+        }
+    }
+}
+
+fun statusToContentColor(status: TaskStatus): Color{
+    return when (status){
+        TaskStatus.TODO -> {
+            Color(0xFFEFF6FF)
+        }
+        TaskStatus.INPROGRESS -> {
+            Color(0xFFFFFBEB)
+        }
+        TaskStatus.DONE -> {
+            Color(0xFFF0FDF4)
+        }
+    }
+}
+
+fun statusToBorderColor(status: TaskStatus): Color{
+    return when (status){
+        TaskStatus.TODO -> {
+            Color(0xFFBEDBFF)
+        }
+        TaskStatus.INPROGRESS -> {
+            Color(0xFFFEE685)
+        }
+        TaskStatus.DONE -> {
+            Color(0xFFB9F8CF)
+        }
+    }
 }
